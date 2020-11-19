d930b542445aa5225764b63bc58a4c458f4e6a85
Krishna Kant Sharma <63699038+krishna-revv@users.noreply.github.com>
2020-11-19 12:09:08 +0530

    PORT fix

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 const express = require("express");
 const path = require("path");
-const cors = require( "cors")
-const imagesToPdf = require("images-to-pdf")
-
+const cors = require("cors");
+const imagesToPdf = require("images-to-pdf");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=> {
-  res.send("Working")
-})
+app.get("/", (req, res) => {
+  res.send("Working");
+});
 
-app.post('/api/canvas/', async (req,res)=>{
-
+app.post("/api/canvas/", async (req, res) => {
   var base64Data = req.body.image_url.replace(/^data:image\/png;base64,/, "");
 
-    require("fs").writeFile("out.png", base64Data, 'base64', async function(err) {
-      await imagesToPdf(["out.png"], "./public/canvas.pdf")
-      res.status(200).json({
-        success:true,
-        data: "canvas.pdf"
-      })
+  require("fs").writeFile("out.png", base64Data, "base64", async function (
+    err
+  ) {
+    await imagesToPdf(["out.png"], "./public/canvas.pdf");
+    res.status(200).json({
+      success: true,
+      data: "canvas.pdf",
     });
-
-   
-
   });
+});
 
-
-const PORT=5000
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
